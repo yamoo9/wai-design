d45861d57729de60bcfcd53e7e977c9ad87c2cb7
Jee Hoon <yamoo9@naver.com>
2016-05-27 07:30:21 +0900

    3일차 수업 내용 정리

--- DAY03/www/js/navigation.js
+++ DAY03/www/js/navigation.js
@@ -1,22 +1,45 @@
 (function(global){
   'use strict';
 
+  // ------------------------------------------------------------------
+  // Initialization
   var $nav = $('.page-navigation');
 
-  $nav.on('mouseenter focus', '.depth1 > li > a', navigationActive);
-  $nav.on('blur', '.depth1 > li:last a', navigationDeactive);
 
+  var $depth1 = $nav.children('ul').addClass('depth1');
+
+  var $depth2 = $depth1.find('ul').addClass('depth2');
+
+  var $allLinks = $nav.find('a');
+
+  settingRoleProperties();
+
+  // ------------------------------------------------------------------
+  // Handler
   function navigationActive(e) {
     allDeactive();
     $(this).parent().addClass('active');
   }
-
   function navigationDeactive(e) {
     allDeactive();
   }
-
   function allDeactive() {
     $nav.find('.depth1 > li').removeClass('active');
   }
+
+  function settingRoleProperties() {
+    // <div role="navigation">
+    if(!$nav.is('nav')) {
+      $nav.attr('role', 'navigation');
+    }
+    $depth1.attr('role', 'menubar');
+    $depth2.attr('role', 'menu');
+    $allLinks.attr('role', 'menu-item');
+  }
+
+  // ------------------------------------------------------------------
+  // Event Binding (Event + Handler)
+  $nav.on('mouseenter focus', '.depth1 > li > a', navigationActive);
+  $nav.on('blur', 'a:last', navigationDeactive);
 
 })(this);
